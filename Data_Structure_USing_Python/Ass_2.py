4c04327f602c4ade8ba20790f55f544ed66f8fd3
Vivekagent47 <vivek.agent47@gmail.com>
2020-02-12 22:00:29 +0530

    Update

--- Data_Structure_USing_Python/Ass_2.py
+++ Data_Structure_USing_Python/Ass_2.py
@@ -1,5 +1,8 @@
-# A positive integer m can be expresseed as the sum of three squares if it is of the form p + q + r where p, q, r ≥ 0, and p, q, r are all perfect squares. For instance, 2 can be written as 0+1+1 but 7 cannot be expressed as the sum of three squares. The first numbers that cannot be expressed as the sum of three squares are 7, 15, 23, 28, 31, 39, 47, 55, 60, 63, 71, … (see Legendre's three-square theorem).
-# Write a Python function threesquares(m) that takes an integer m as input and returns True if m can be expressed as the sum of three squares and False otherwise. (If m is not positive, your function should return False.)
+# A positive integer m can be expresseed as the sum of three squares if it is of the form p + q + r where p, q, r ≥ 0, and p, q, r are all perfect squares. 
+# For instance, 2 can be written as 0+1+1 but 7 cannot be expressed as the sum of three squares. 
+# The first numbers that cannot be expressed as the sum of three squares are 7, 15, 23, 28, 31, 39, 47, 55, 60, 63, 71, … (see Legendre's three-square theorem).
+# Write a Python function threesquares(m) that takes an integer m as input and returns True if m can be expressed as the sum of three squares and False otherwise. 
+# (If m is not positive, your function should return False.)
 # Here are some examples of how your function should work.
 # >>> threesquares(6)
 # True
@@ -40,12 +43,13 @@
 
 
 def threesquares(n):
-  for i in range(0,n):
-    for j in range(0,n):
-      for k in range(0,n):
-        if i**2 + j**2 + k**2 == n:
-          return True
-  return False
+    if n >= 0:
+        for i in range(0,n):
+            for j in range(0,n):
+                for k in range(0,n):
+                    if i**2 + j**2 + k**2 == n:
+                        return True
+    return False
 
 
 def repfree(s):

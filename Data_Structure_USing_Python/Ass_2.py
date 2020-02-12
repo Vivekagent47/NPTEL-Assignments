96571e675e76a08153c58f294b0d339e138b4a7f
Vivekagent47 <vivek.agent47@gmail.com>
2020-02-12 21:56:57 +0530

    Question Added

--- Data_Structure_USing_Python/Ass_2.py
+++ Data_Structure_USing_Python/Ass_2.py
@@ -1,3 +1,44 @@
+# A positive integer m can be expresseed as the sum of three squares if it is of the form p + q + r where p, q, r ≥ 0, and p, q, r are all perfect squares. For instance, 2 can be written as 0+1+1 but 7 cannot be expressed as the sum of three squares. The first numbers that cannot be expressed as the sum of three squares are 7, 15, 23, 28, 31, 39, 47, 55, 60, 63, 71, … (see Legendre's three-square theorem).
+# Write a Python function threesquares(m) that takes an integer m as input and returns True if m can be expressed as the sum of three squares and False otherwise. (If m is not positive, your function should return False.)
+# Here are some examples of how your function should work.
+# >>> threesquares(6)
+# True
+# >>> threesquares(188)
+# False
+# >>> threesquares(1000)
+# True
+
+
+
+
+# Write a function repfree(s) that takes as input a string s and checks whether any character appears more than once. The function should return True if there are no repetitions and False otherwise.
+# Here are some examples to show how your function should work.
+# >>> repfree("zb%78")
+# True
+# >>> repfree("(7)(a")
+# False
+# >>> repfree("a)*(?")
+# True
+# >>> repfree("abracadabra")
+# False
+
+
+
+
+# A list of numbers is said to be a hill if it consists of an ascending sequence followed by a descending sequence, where each of the sequences is of length at least two. Similarly, a list of numbers is said to be a valley hill if it consists of an descending sequence followed by an ascending sequence. You can assume that consecutive numbers in the input sequence are always different from each other.
+# Write a Python function hillvalley(l) that takes a list l of integers and returns True if it is a hill or a valley, and False otherwise.
+# Here are some examples to show how your function should work.
+# >>> hillvalley([1,2,3,5,4])
+# True
+# >>> hillvalley([1,2,3,4,5])
+# False
+# >>> hillvalley([5,4,1,2,3])
+# True
+# >>> hillvalley([5,4,3,2,1])
+# False
+
+
+
 def threesquares(n):
   for i in range(0,n):
     for j in range(0,n):
